f68b862d59d5f084333fcec9a1db69659575fe80
markus.reimann <markus.reimann@khk.ee>
2017-03-15 15:32:57 +0200

    Koostatud on rakenduse konfigutasioon. Esma versioon.

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markus.reimann
+ * Date: 15.03.2017
+ * Time: 15:25
+ */
+// defineerime vajalikud konstandid
+define('CLASSES_DIR', 'classes/'); //classes kataloogi konstant
+define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi konstant
+?>
--- index.php
+++ index.php
@@ -5,5 +5,8 @@
  * Date: 15.03.2017
  * Time: 13:08
  */
+// võtame konfiguratsiooni kasutusele
+require_once 'conf.php';
+// pea lehe sisu
 echo '<h1>Veebiprogrammeerimise esileht</h1>';
 ?>
