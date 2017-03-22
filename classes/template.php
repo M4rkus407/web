71bc6512ae92f2d9ba3991a5cd9b37b17f25ff66
markus.reimann <markus.reimann@khk.ee>
2017-03-22 12:48:38 +0200

    Lisatud mall faili kontrollimine ja laadimine

--- classes/template.php
+++ classes/template.php
@@ -13,6 +13,25 @@ class template
     var $content = false; //html mall faili sisu
     var $vars = ''; //html vaate sisu- reaalsed väärtused
     //Klassi tegevused- meetodid- funktsioonid
+
+    //Html mall faili lugemine
+    function loadFile(){
+        $f = $this->file;//Lokaalne asendus
+        if(!is_dir(TMPL_DIR)) {//Kontrollime mallide kausta olemasolu
+            echo 'Kataloogi'.TMPL_DIR.'ei leitud</ br>';
+            exit;
+        }
+        //Kui fail on olemas ja lugemiseks sobiv
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            //Loeme failist malli sisu
+            $this->readFile($f);
+        }
+        //Kui sisu ei olnud võimalik lugeda
+        if($this->content == false){
+            echo 'Ei suutnud lugeda faili'.$this->file.'</ br>';
+        }
+    }//Loadfile lõpp
+
     //Loeme sisu html malli failist
     function readFile($f){
         $this->content = file_get_contents($f);
