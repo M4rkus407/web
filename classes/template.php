676bb29a86f458bc74c6d0cbceeba89695499335
markus.reimann <markus.reimann@khk.ee>
2017-03-22 12:21:27 +0200

    esimene versioon klassi template'ist

---
+++ classes/template.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: markus.reimann
+ * Date: 22.03.2017
+ * Time: 12:11
+ */
+class template
+{ //Klassi algus
+    // template klassi omadused-muutujad
+    var $file = ''; //html mall faili nimi
+    var $content = false; //html mall faili sisu
+    var $vars = ''; //html vaate sisu- reaalsed väärtused
+}//Klassi lõpp
