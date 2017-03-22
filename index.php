8ea6486a6cd0c64f7c2b05e4b4181a8ba0dc2eaa
markus.reimann <markus.reimann@khk.ee>
2017-03-22 14:37:21 +0200

    Määratud pealehe reaalsed väärtused

--- index.php
+++ index.php
@@ -12,6 +12,12 @@ echo '<h1>Veebiprogrammeerimise esileht</h1>';
 
 //Valmistame peatemplate objekti
 $main_tmpl = new template('main');
+//Valmistame paarid malli_element => väärtus
+$main_tmpl->set('user', 'Kasutajanimi');
+$main_tmpl->set('title', 'Pealeht');
+$main_tmpl->set('lang_bar', 'Keeleriba');
+$main_tmpl->set('menu', 'Lehe peamenüü');
+$main_tmpl->set('content', 'Lehe sisu');
 //Kontrollin  antud objekti sisu
 echo '<pre>';
 print_r($main_tmpl);
