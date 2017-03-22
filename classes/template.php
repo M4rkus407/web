e0a6cf25eabe23141da548bb9d3578d8350bb27e
markus.reimann <markus.reimann@khk.ee>
2017-03-22 12:29:22 +0200

    Lisatud failist lugemine

--- classes/template.php
+++ classes/template.php
@@ -12,4 +12,9 @@ class template
     var $file = ''; //html mall faili nimi
     var $content = false; //html mall faili sisu
     var $vars = ''; //html vaate sisu- reaalsed väärtused
+    //Klassi tegevused- meetodid- funktsioonid
+    //Loeme sisu html malli failist
+    function readFile($f){
+        $this->content = file_get_contents($f);
+    }//ReadFile'i lõpp
 }//Klassi lõpp
