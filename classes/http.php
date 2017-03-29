2c790a6c60c0144815788b60f5f891b6aa6ac3f4
markus.reimann <markus.reimann@khk.ee>
2017-03-29 14:13:06 +0300

    Lisatud vajalikke konstandite loomine

--- classes/http.php
+++ classes/http.php
@@ -12,10 +12,22 @@ class http
     var $vars = array(); //hhtp päringute andmed
     var $server = array(); //serveri masina andmed
     //Klassi meetodid
+
     //Paneme paika algandmed
     function init(){
         $this->vars = array_merge($_GET, $_POST, $_FILES);
         $this->server = $_SERVER;
     }//Init
+
+    //defineerime vajalikud konstandid
+    function initCont(){
+        $consts = array('REMOTE_ADDR', 'HTTP_HOST', 'PHP_SELF', 'SCRIPT_NAME');
+        foreach ($consts as $const){
+            if(!defined($const) and isset($this->server[$const])){
+                define($const, $this->server[$const]);
+            }
+        }
+    }//INIT CONST
+
 } //Klassi lõpp
 ?>
