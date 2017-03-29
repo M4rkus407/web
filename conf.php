9ced88a0704d59588cf544bc54d7a8538cd076d2
markus.reimann <markus.reimann@khk.ee>
2017-03-29 14:51:31 +0300

    Lisatud lib kataloogis utils kasutamine

--- conf.php
+++ conf.php
@@ -8,14 +8,22 @@
 // defineerime vajalikud konstandid
 define('CLASSES_DIR', 'classes/'); //classes kataloogi konstant
 define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi konstant
+define('LIB_DIR', 'lib/');
+
+//Võtame kasutusse vajlikud abi failid
+require_once LIB_DIR.'utils.php';
 
 //Võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
 require_once CLASSES_DIR.'linkobject.php';
+
 //Loome vajalikud objektid
 $http = new linkobject();
 //Testime link objekti tööd
 
 echo $http->baseUrl;
+//echo '<pre>';
+//print_r($http)
+//echo '</pre>';
 ?>
