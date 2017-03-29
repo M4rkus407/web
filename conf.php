bcc4a4ce0d15591e9ffed2aada1fba241c6ae5c5
markus.reimann <markus.reimann@khk.ee>
2017-03-29 13:57:49 +0300

    Lisatud http objekti loomine ja testimine

--- conf.php
+++ conf.php
@@ -12,4 +12,12 @@ define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi konstant
 //Võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
+
+//Loome vajalikud objektid
+$http = new http();
+$http->init();
+//Testime http objekti tööd
+echo '<pre>';
+print_r($http);
+echo '</pre>';
 ?>
