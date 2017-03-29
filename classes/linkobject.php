808c610dcba23f24a0b3174d1762bba0e59346b5
markus.reimann <markus.reimann@khk.ee>
2017-03-29 14:35:35 +0300

    Lisatud põhilingi loomise võimalused

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -8,5 +8,17 @@
  */
 class linkobject extends http
 { //Klassi algus
+    //Klassi muutujad- omadused
+    var $baseUrl = false;
+    var $delim = '&amp;';
+    var $eq = '=';
+    var $protocol = 'http://';
 
+    //Klassi meetodid
+    //Klassi konstruktor algus
+    function __construct(){
+        parent::__construct();//Kutsume tööle http konstruktori
+        //Loome põhi link'i
+        $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }//Konstruktori lõpp
 } //Klassi lõpp
