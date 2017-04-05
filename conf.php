d5619f7fcca95f9432ace96b38ca0ecd40d1e486
markus.reimann <markus.reimann@khk.ee>
2017-04-05 11:39:32 +0300

    Koostatud mysql klassi algversiooni testimine

--- conf.php
+++ conf.php
@@ -8,19 +8,28 @@
 // defineerime vajalikud konstandid
 define('CLASSES_DIR', 'classes/'); //classes kataloogi konstant
 define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi konstant
-define('LIB_DIR', 'lib/');
+define('LIB_DIR', 'lib/'); //Lib kataloogi nime konstant
 define('ACTS_DIR', 'acts/'); //acts kataloogi nime konstant
 define('DEFAULT_ACT', 'default'); //vaikimisi tegevuse  faili nime konstant
 
 //Võtame kasutusse vajlikud abi failid
 require_once LIB_DIR.'utils.php';
+require_once 'db_conf.php';
 
 //Võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
 require_once CLASSES_DIR.'linkobject.php';
+require_once CLASSES_DIR.'mysql.php';
 
 //Loome vajalikud objektid
 $http = new linkobject();
+$db = new mysql(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//testime  andmebaasi objekti
+$res = $db->query('SELECT NOW()');
+echo '<pre>';
+print_r($res);
+echo '</pre>';
+
 
 ?>
