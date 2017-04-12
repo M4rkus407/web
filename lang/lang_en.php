52ff391345af79f66fe6b731fe23ee521072bb47
markus.reimann <markus.reimann@khk.ee>
2017-04-12 13:07:18 +0300

    Lisatud inglise keele tõlkimine

---
+++ lang/lang_en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markus.reimann
+ * Date: 12.04.2017
+ * Time: 13:00
+ */
+$_trans = array(
+    'Esimene' => 'First',
+    'Teine' => 'Second';
+    'Minu lehe pealkiri' => 'My page heading',
+    'Logi välja' => 'Logout',
+    'Kasutajanimi' => 'Username',
+    'Parool' => 'Password',
+    'Logi sisse' => 'Login',
+    'Viga sisselogimisel' => 'Login error'
+);
+
+?>
