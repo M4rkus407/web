d8c57049f3d267bf9acac623796af2bfda27ed14
markus.reimann <markus.reimann@khk.ee>
2017-04-19 09:49:46 +0300

    Koostatud sessiooni id loomine ja andmete salvestamine adnmebaasi

--- classes/session.php
+++ classes/session.php
@@ -22,4 +22,34 @@ class session
         $this->http = &$http;
         $this->db = &$db;
     }//Konstruktor lõpp
+
+    //Sessiooni loomine algus
+    function createSession($user = false){
+        //Kui kasutaja on anonüüme
+        if($user == false){
+            //Tekitame andmed session tabeli jaoks - algus
+            $user = array(
+                'user_id' => 0,
+                'role_id' => 0,
+                'username' => 'Anonymous'
+            );
+        }//Anonüümne kasutaja lõpp
+        //Unikaalse sessiooni id loomine
+        $sid = md5(uniqid(time().mt_rand(1, 1000), true));
+        //päring sessiooni andmete salvestamiseks andmebaasi
+        $sql ='INSERT INTO session SET'.
+            'sid='.fixDb($sid).', '.
+            'user_id='.fixDb($user['user_id']).', '.
+            'user_data='.fixDb(serialize($user)).', '.
+            'login_ip='.fixDb(REMOTE_ADDR).', '.
+            'created=NOW()';
+        //Sisestame päringu andmebaasi
+        $this->db->query($sql);
+        //määrame sid ka antud klassi muutujale var $sid
+        $this->sid =$sid;
+        //paneme antud väärtuse ka veebi - lehtede vahel kasutamiseks
+        $this->http->set('sid', $sid);
+
+
+    }//Sessiooni loomine lõpp
 }//Klassi lõpp
