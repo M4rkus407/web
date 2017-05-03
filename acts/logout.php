4d56f7294f6b45832d8933e79006b33119fe014d
markus.reimann <markus.reimann@khk.ee>
2017-05-03 11:09:48 +0300

    Lisatud kasutaja väljalogimine

---
+++ acts/logout.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markus.reimann
+ * Date: 3.05.2017
+ * Time: 11:02
+ */
+$sess->delsession(); //kustutame olemasoleva sessiooni
+$http->redirect(); //suuname avalehele
