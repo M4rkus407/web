8c9986836a0e4c0367ebe4ed10c179c8f4c2f448
markus.reimann <markus.reimann@khk.ee>
2017-05-03 11:20:26 +0300

    Eemaldatud vigade näitamine

--- conf.php
+++ conf.php
@@ -5,6 +5,8 @@
  * Date: 15.03.2017
  * Time: 15:25
  */
+error_reporting(0);
+
 // defineerime vajalikud konstandid
 define('CLASSES_DIR', 'classes/'); //classes kataloogi konstant
 define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi konstant
